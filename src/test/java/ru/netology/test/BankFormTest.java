e0a622b42894d0c2c1f78ae3c8ae5c4716a134f1
Tatiana Titova <ritasom666@gmail.com>
2024-03-27 19:27:57 +0400

    add ex.1

--- src/test/java/ru/netology/test/BankFormTest.java
+++ src/test/java/ru/netology/test/BankFormTest.java
@@ -1,26 +1,26 @@
 package ru.netology.test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import java.util.List;
 
 
 public class BankFormTest {
     private WebDriver driver;
+
     @BeforeAll
-            static void setUpAll(){
+    static void setUpAll() {
         WebDriverManager.chromedriver().setup();
     }
+
     @BeforeEach
-            void setUp(){
+    void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
@@ -29,16 +29,24 @@ public class BankFormTest {
     }
 
     @AfterEach
-    void tearDown(){
+    void tearDown() {
         driver.quit();
         driver = null;
     }
 
     @Test
-    void shouldLoad(){
+    void shouldSuccessSendForm() {
         driver.get("http://localhost:9999/");
         WebElement form = driver.findElement(By.cssSelector
                 ("[class=\"form form_size_m form_theme_alfa-on-white\"]"));
-        List<WebElement> elements = driver.findElements(By.className("input_view_default"));
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Иван Иванов");
+        elements.get(1).sendKeys("+79160000000");
+        form.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
+        form.findElement(By.className("button_view_extra")).click();
+        String text = driver.findElement(By.cssSelector
+                ("[data-test-id=\"order-success\"]")).getText();
+        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",
+                text.trim());
     }
 }
