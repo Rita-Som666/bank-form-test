e9ac118f1c9efced9e8d95203cb5d3fe4f268de8
Tatiana Titova <ritasom666@gmail.com>
2024-03-29 21:54:06 +0400

    ex.2

--- src/test/java/ru/netology/test/BankFormTest.java
+++ src/test/java/ru/netology/test/BankFormTest.java
@@ -35,18 +35,103 @@ public class BankFormTest {
     }
 
     @Test
-    void shouldSuccessSendForm() {
+    void shouldSuccessSendFormAndFieldNamePositiveValidation() {
         driver.get("http://localhost:9999/");
-        WebElement form = driver.findElement(By.cssSelector
-                ("[class=\"form form_size_m form_theme_alfa-on-white\"]"));
         List<WebElement> elements = driver.findElements(By.className("input__control"));
         elements.get(0).sendKeys("Иван Иванов");
         elements.get(1).sendKeys("+79160000000");
-        form.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
-        form.findElement(By.className("button_view_extra")).click();
+        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
+        driver.findElement(By.className("button_view_extra")).click();
         String text = driver.findElement(By.cssSelector
                 ("[data-test-id=\"order-success\"]")).getText();
         Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",
                 text.trim());
+        driver.navigate().refresh();
+        elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Иван-Иванов");
+        elements.get(1).sendKeys("+79160000000");
+        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
+        driver.findElement(By.className("button_view_extra")).click();
+        text = driver.findElement(By.cssSelector
+                ("[data-test-id=\"order-success\"]")).getText();
+        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",
+                text.trim());
+        driver.navigate().refresh();
+        elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Иван");
+        elements.get(1).sendKeys("+79160000000");
+        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
+        driver.findElement(By.className("button_view_extra")).click();
+        text = driver.findElement(By.cssSelector
+                ("[data-test-id=\"order-success\"]")).getText();
+        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",
+                text.trim());
+        driver.navigate().refresh();
+        elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Иван-Иванов Иванович");
+        elements.get(1).sendKeys("+79160000000");
+        driver.findElement(By.cssSelector("[data-test-id=\"agreement\"]")).click();
+        driver.findElement(By.className("button_view_extra")).click();
+        text = driver.findElement(By.cssSelector
+                ("[data-test-id=\"order-success\"]")).getText();
+        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",
+                text.trim());
+
+    }
+
+    @Test
+    void fieldNameNegativeValidation(){
+        driver.get("http://localhost:9999/");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Ivan Ivanov");
+        driver.findElement(By.className("button_view_extra")).click();
+        driver.findElement(By.cssSelector(".input_invalid[data-test-id=\"name\"]"));
+        String text = driver.findElement(By.className("input__sub")).getText();
+        Assertions.assertEquals
+                ("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.",
+                text.trim());
+        driver.navigate().refresh();
+        elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Иван1");
+        driver.findElement(By.className("button_view_extra")).click();
+        driver.findElement(By.cssSelector(".input_invalid[data-test-id=\"name\"]"));
+        text = driver.findElement(By.className("input__sub")).getText();
+        Assertions.assertEquals
+                ("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.",
+                        text.trim());
+        driver.navigate().refresh();
+        elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Иван.Иванов");
+        driver.findElement(By.className("button_view_extra")).click();
+        driver.findElement(By.cssSelector(".input_invalid[data-test-id=\"name\"]"));
+        text = driver.findElement(By.className("input__sub")).getText();
+        Assertions.assertEquals
+                ("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.",
+                        text.trim());
+        driver.navigate().refresh();
+        elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("");
+        driver.findElement(By.className("button_view_extra")).click();
+        driver.findElement(By.cssSelector(".input_invalid[data-test-id=\"name\"]"));
+        text = driver.findElement(By.className("input__sub")).getText();
+        Assertions.assertEquals
+                ("Поле обязательно для заполнения",
+                        text.trim());
+        driver.navigate().refresh();
+
+    }
+
+    @Test
+    void fieldPhoneNegativeValidation() {
+        driver.get("http://localhost:9999/");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Иван Иванов");
+        elements.get(1).sendKeys("0000");
+        driver.findElement(By.className("button_view_extra")).click();
+
+        String text = driver.findElement(By.cssSelector(".input_invalid[data-test-id='phone'] .input__sub")).getText();
+        Assertions.assertEquals
+                ("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.",
+                        text.trim());
     }
 }
